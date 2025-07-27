3102cae9fdfc062c9e49e2516bc0fcd373542da9
Oleksandr <rachok010291@gmail.com>
2025-07-27 21:34:31 +0100

    update files

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "node:path";
+
 import { fileURLToPath } from "node:url";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
+  base: "/02-react-cafe/",
   plugins: [react()],
   resolve: {
     alias: {
